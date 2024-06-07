c659da44a5c68b8aa433d1078451bea39f040c49
87003697 <870036797@qq.com>
2024-06-07 21:12:57 +0800

    update_quaplan

--- models/geometry/__init__.py
+++ models/geometry/__init__.py
@@ -1,5 +1,6 @@
 from . import (
     triplane_transformer,
     hypernetwork,
-    one_step_triplane_stable_diffusion
+    one_step_triplane_stable_diffusion,
+    one_step_quaplane_stable_diffusion
 )
