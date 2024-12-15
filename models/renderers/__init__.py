ab4e11ecf766c0dfb4548fde1bf45883f15efd6c
theEricMa <zm2354@columbia.edu>
2024-12-15 13:17:07 +0000

    update

--- models/renderers/__init__.py
+++ models/renderers/__init__.py
@@ -2,4 +2,5 @@ from . import (
     generative_space_volsdf_volume_renderer,
     generative_space_dmtet_rasterize_renderer,
     generative_space_diffmc_rasterize_renderer,
+    generative_space_occ_neus_renderer,
 )
