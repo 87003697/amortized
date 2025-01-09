c5703dea2732b67b445ae4bbc719ac056c09fc3b
theEricMa <zm2354@columbia.edu>
2025-01-09 12:07:20 +0000

    update

--- data/__init__.py
+++ data/__init__.py
@@ -6,4 +6,5 @@ from . import (
     multiview_multiprompt_dualrender_multistep,
     # multiview_multiref_dualrender_multistep,
     multiview_multiprompt_dualrender_multistep_v2,
+    multiview_multiprompt_dualrender_multistep_v2_ortho,
 )
