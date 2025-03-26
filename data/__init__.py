f89ca0f9daa7e27abfe3a5078d1f3cd9b05d7f3a
87003697 <870036797@qq.com>
2025-03-26 11:50:04 +0800

    update

--- data/__init__.py
+++ data/__init__.py
@@ -5,6 +5,7 @@ from . import (
     multiview_multiprompt_dualrender_semisup,
     multiview_multiprompt_dualrender_multistep,
     multiview_multiprompt_dualrender_multistep_v2,
+    multiview_multiprompt_dualrender_multistep_v2_fix,
     # multiview_multiprompt_dualrender_multistep_v2_ortho,
     multiview_multiimage_dualrender_multistep_v2_wonder3D,
 )
